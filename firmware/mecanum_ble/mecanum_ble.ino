78502b13c1e049ecf4a51437187acb0e1c8df660
Isfandiyor Akbarov <isfandiyorakbarov450@gmail.com>
2026-09-06 20:26:54 +0500

    fix

--- firmware/mecanum_ble/mecanum_ble.ino
+++ firmware/mecanum_ble/mecanum_ble.ino
@@ -1,4 +1,4 @@
-#define MAX_SPEED 170  // максимальная скорость моторов (0-255)
+#define MAX_SPEED 200  // максимальная скорость моторов (0-255)
 
 #define MOTOR_TEST 1  // тест моторов
 // при запуске крутятся ВПЕРЁД по очереди:
@@ -8,14 +8,14 @@
 // BR - задний правый
 
 // пины драйверов (_B должен быть ШИМ)
-#define MOTOR1_A 0
-#define MOTOR1_B 1  // ШИМ!
+#define MOTOR1_A 1
+#define MOTOR1_B 0  // ШИМ!
 
 #define MOTOR2_A 3
 #define MOTOR2_B 4  // ШИМ!
 
-#define MOTOR3_A 21
-#define MOTOR3_B 20  // ШИМ!
+#define MOTOR3_A 20
+#define MOTOR3_B 21  // ШИМ!
 
 #define MOTOR4_A 7
 #define MOTOR4_B 6  // ШИМ!
@@ -24,11 +24,11 @@
 #include <GyverMotor2.h>
 
 ControllerPtr myControllers[BP32_MAX_GAMEPADS];
-ControllerPtr gamepad;
+ControllerPtr gamepad = nullptr;
 
-GyverMotor2<GM2::DIR_PWM_INV> motorBL(MOTOR1_A, MOTOR1_B);
-GyverMotor2<GM2::DIR_PWM_INV> motorFL(MOTOR2_A, MOTOR2_B);
-GyverMotor2<GM2::DIR_PWM_INV> motorFR(MOTOR3_A, MOTOR3_B);
+GyverMotor2<GM2::DIR_PWM_INV> motorFL(MOTOR1_A, MOTOR1_B);
+GyverMotor2<GM2::DIR_PWM_INV> motorFR(MOTOR2_A, MOTOR2_B);
+GyverMotor2<GM2::DIR_PWM_INV> motorBL(MOTOR3_A, MOTOR3_B);
 GyverMotor2<GM2::DIR_PWM_INV> motorBR(MOTOR4_A, MOTOR4_B);
 
 void setup() {
@@ -50,36 +50,37 @@ void setup() {
 
 #if (MOTOR_TEST == 1)
   Serial.println("front left");
-  motorFL.runSpeed(100);
+  motorFL.runSpeed(150);
   delay(3000);
   motorFL.stop();
   delay(1000);
   Serial.println("front right");
-  motorFR.runSpeed(100);
+  motorFR.runSpeed(150);
   delay(3000);
   motorFR.stop();
   delay(1000);
   Serial.println("back left");
-  motorBL.runSpeed(100);
+  motorBL.runSpeed(150);
   delay(3000);
   motorBL.stop();
   delay(1000);
   Serial.println("back right");
-  motorBR.runSpeed(100);
+  motorBR.runSpeed(150);
   delay(3000);
   motorBR.stop();
 #endif
+
   // минимальный сигнал на мотор
-  motorFR.setMinDuty(30);
-  motorBR.setMinDuty(30);
-  motorFL.setMinDuty(30);
-  motorBL.setMinDuty(30);
+  motorFR.setMinDuty(80);
+  motorBR.setMinDuty(80);
+  motorFL.setMinDuty(80);
+  motorBL.setMinDuty(80);
 
   // скорость плавности
-  motorFR.setAccel(60);
-  motorBR.setAccel(60);
-  motorFL.setAccel(60);
-  motorBL.setAccel(60);
+  motorFR.setAccel(150);
+  motorBR.setAccel(150);
+  motorFL.setAccel(150);
+  motorBL.setAccel(150);
 }
 
 void loop() {
@@ -93,14 +94,22 @@ void loop() {
         gamepad->axisX(),   // (-511 - 512) left X Axis
         gamepad->axisY(),   // (-511 - 512) left Y axis
         gamepad->axisRX(),  // (-511 - 512) right X axis
-        gamepad->axisRY(),  // (-511 - 512) right Y axis
+        gamepad->axisRY()   // (-511 - 512) right Y axis
       );
 
       // переводим диапазон 0..255 в -MAX_SPEED..MAX_SPEED
       int valLX = map(gamepad->axisX(), -511, 512, -MAX_SPEED, MAX_SPEED);
-      int valLY = map(gamepad->axisY(), -511, 512, -MAX_SPEED, MAX_SPEED);
+      int valLY = map(gamepad->axisY(), 512, -511, -MAX_SPEED, MAX_SPEED);
       int valRX = map(gamepad->axisRX(), -511, 512, -MAX_SPEED, MAX_SPEED);
-      int valRY = map(gamepad->axisRY(), -511, 512, -MAX_SPEED, MAX_SPEED);
+      int valRY = map(gamepad->axisRY(), 512, -511, -MAX_SPEED, MAX_SPEED);
+
+      Serial.printf(
+        "LX: %4d, LY: %4d, RX: %4d, RY: %4d\n",
+        valLX,  // (-511 - 512) left X Axis
+        valLY,  // (-511 - 512) left Y axis
+        valRX,  // (-511 - 512) right X axis
+        valRY   // (-511 - 512) right Y axis
+      );
 
       int dutyFR = valLY + valLX;
       int dutyFL = valLY - valLX;
@@ -112,20 +121,33 @@ void loop() {
       dutyBR += valRY - valRX;
       dutyBL += valRY + valRX;
 
+      Serial.printf(
+        "FR: %4d, FL: %4d, BR: %4d, BL: %4d\n\n",
+        dutyFR,  // (-511 - 512) left X Axis
+        dutyFL,  // (-511 - 512) left Y axis
+        dutyBR,  // (-511 - 512) right X axis
+        dutyBL   // (-511 - 512) right Y axis
+      );
+
       // ПЛАВНЫЙ контроль скорости, защита от рывков
-      motorFR.setSpeed(dutyFR);
-      motorBR.setSpeed(dutyBR);
-      motorFL.setSpeed(dutyFL);
-      motorBL.setSpeed(dutyBL);
+      motorFR.runSpeed(dutyFR);
+      motorBR.runSpeed(dutyBR);
+      motorFL.runSpeed(dutyFL);
+      motorBL.runSpeed(dutyBL);
     }
   } else {
     // страшно, вырубай
-    motorFR.setSpeed(0);
-    motorBR.setSpeed(0);
-    motorFL.setSpeed(0);
-    motorBL.setSpeed(0);
+    motorFR.stop();
+    motorBR.stop();
+    motorFL.stop();
+    motorBL.stop();
   }
-  delay(50);
+  motorFR.tick();
+  motorBR.tick();
+  motorFL.tick();
+  motorBL.tick();
+
+  delay(20);
 }
 
 void onConnectedController(ControllerPtr ctl) {
@@ -152,7 +174,7 @@ void onConnectedController(ControllerPtr ctl) {
 void onDisconnectedController(ControllerPtr ctl) {
   bool foundController = false;
 
-  if (controller = ctl) gamepad = nullptr;
+  if (gamepad == ctl) gamepad = nullptr;
 
   for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
     if (myControllers[i] == ctl) {
